7080fb5b49c2da9c7b3936b0e373aea022704af3
dhhAndroid <dhhmvp@163.com>
2016-10-15 15:36:09 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/duhuihui/pulltorefreshrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/duhuihui/pulltorefreshrecyclerview/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements PulltoRefreshRecy
         refreshRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
         refreshRecyclerView.setRefreshLoadMoreListener(this);
         adapter = new MyAdapter(list);
+        //发生大幅
         refreshRecyclerView.setAdapter(adapter);
     }
 
